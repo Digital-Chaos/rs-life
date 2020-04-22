abad11fefd0790a22c5f4158b16c71e06be0ba5b
James Wright <james.wright@digital-chaos.com>
2020-04-22 18:46:55 +0100

    Split into main.rs and life.rs, unit tests, added Grid and Cell structs/impls

---
+++ src/life.rs
@@ -0,0 +1,235 @@
+/* life.rs - John Conway's Game of Life Cellular Automata
+*  (c)2020 James Wright, see LICENSE file.
+*/
+
+extern crate rand;
+
+use std::fmt;
+use rand::Rng;
+
+/*****************************************************************************/
+
+pub struct Cell {
+    alive:  bool
+}
+
+impl Cell {
+    const EMPTY_CELL:   char = ' ';
+    const LIVE_CELL:    char = 'O';
+
+    // Return next cell state
+    pub fn next(&self, neighbours: u8) -> Cell {
+        Cell { alive: (neighbours == 3) || ((neighbours == 2) && self.alive) }
+    }
+
+    // Map cell state to a char
+    pub fn to_char(&self) -> char {
+        if self.alive { Cell::LIVE_CELL } else { Cell::EMPTY_CELL }
+    }
+}
+
+impl PartialEq for Cell {
+    fn eq(&self, other: &Self) -> bool {
+        self.alive == other.alive
+    }
+}
+
+impl fmt::Debug for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Cell")
+         .field("alive", &self.alive)
+         .finish()
+    }
+}
+
+/*****************************************************************************/
+
+pub struct Grid {
+    cells:  Vec<Vec<Cell>>
+}
+
+impl Grid {
+    // Return a Grid of randomised cell states
+    pub fn random<R: Rng>(mut rng: R, width: usize, height: usize) -> Grid {
+        Grid {
+            cells:  (0..height)
+                    .map(|_| (0..width).map(|_| Cell { alive: (rng.gen::<u32>() & 1) != 0 }).collect())
+                    .collect()
+        }
+    }
+
+    // Return next Grid state
+    pub fn next<F: Fn(&Cell, u8)->Cell>(&self, cell_func: F) -> Grid {
+        Grid {
+            cells:  self.cells.iter().enumerate()
+                    .map(|(y, row)| row.iter().enumerate()
+                        .map(|(x, cell)| cell_func(&cell, self.neighbours(x, y)))
+                        .collect())
+                    .collect()
+        }
+    }
+
+    // Return number of neighbours for given cell position
+    fn neighbours(&self, x: usize, y: usize) -> u8 {
+        let cells  = &self.cells;
+        let left   = Grid::index_wrap(x as isize - 1, cells[0].len());  // wrap to row width
+        let right  = Grid::index_wrap(x as isize + 1, cells[0].len());  // wrap to row width
+        let top    = Grid::index_wrap(y as isize - 1, cells.len());     // wrap to grid height
+        let bottom = Grid::index_wrap(y as isize + 1, cells.len());     // wrap to grid height
+
+        (cells[top][left].alive    as u8) + (cells[top][x].alive    as u8) + (cells[top][right].alive    as u8) +
+        (cells[y][left].alive      as u8) +                                  (cells[y][right].alive      as u8) +
+        (cells[bottom][left].alive as u8) + (cells[bottom][x].alive as u8) + (cells[bottom][right].alive as u8)
+    }
+
+    // Return wrapped index making sure it's within range [0..len)
+    fn index_wrap(i: isize, len: usize) -> usize {
+        (i + len as isize) as usize % len
+    }
+}
+
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", 
+            self.cells.iter()
+            .map(|row| row.iter().map(Cell::to_char).collect::<String>() )
+            .collect::<Vec<String>>()
+            .join("\r\n"))
+    }
+}
+
+/*****************************************************************************/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::rngs::mock::StepRng;
+
+    const CELL_DEAD:    Cell = Cell { alive: false };
+    const CELL_ALIVE:   Cell = Cell { alive: true  };
+
+    #[test]
+    fn random_should_generate_grid_with_radomnly_populated_cells() {
+        // given
+        let width:  usize = 5;
+        let height: usize = 4;
+        let rng = StepRng::new(0, 1);
+
+        // when
+        let grid = Grid::random(rng, width, height);
+        
+        // then
+        assert_eq!(grid.cells.len(), height, "Grid should have correct # of rows");
+        assert_eq!(grid.cells[0].len(), width, "Grid should have correct # of cells in a row");
+        assert_eq!(grid.cells.iter().any(|row| row.len() != width), false);
+        assert_eq!(grid.cells.iter()
+            .map(|row| row.iter().filter(|&cell| cell.alive == true).count())
+            .sum::<usize>(), 
+            (width * height) / 2, "Half of grid cells should be alive");
+    }
+
+
+    #[test]
+    fn next_should_return_next_grid_given_cell_inversion_function() {
+        // given
+        fn cell_function(cell: &Cell, _neighbours: u8) -> Cell  { Cell { alive: !cell.alive } };
+        let grid = Grid {
+            cells:  vec!(vec!(CELL_DEAD, CELL_DEAD,  CELL_DEAD),
+                         vec!(CELL_DEAD, CELL_ALIVE, CELL_DEAD),
+                         vec!(CELL_DEAD, CELL_DEAD,  CELL_DEAD))
+        };
+
+        // when
+        let next_grid = grid.next(cell_function); 
+
+        // then
+        assert_eq!(next_grid.cells.len(),       grid.cells.len());
+        assert_eq!(next_grid.cells[0].len(),    grid.cells[0].len());
+        assert_eq!(next_grid.cells,             vec!(vec!(CELL_ALIVE, CELL_ALIVE,  CELL_ALIVE),
+                                                     vec!(CELL_ALIVE, CELL_DEAD,   CELL_ALIVE),
+                                                     vec!(CELL_ALIVE, CELL_ALIVE,  CELL_ALIVE)));
+    }
+
+    #[test]
+    fn next_should_return_next_grid_given_cell_neighbour_function() {
+        // given
+        fn cell_function(_cell: &Cell, neighbours: u8) -> Cell  { Cell { alive: neighbours == 8 } };
+        let grid = Grid {
+            cells:  vec!(vec!(CELL_ALIVE, CELL_ALIVE,  CELL_ALIVE),
+                         vec!(CELL_ALIVE, CELL_DEAD,   CELL_ALIVE),
+                         vec!(CELL_ALIVE, CELL_ALIVE,  CELL_ALIVE))
+        };
+
+        // when
+        let next_grid = grid.next(cell_function); 
+
+        // then
+        assert_eq!(next_grid.cells.len(),       grid.cells.len());
+        assert_eq!(next_grid.cells[0].len(),    grid.cells[0].len());
+        assert_eq!(next_grid.cells,             vec!(vec!(CELL_DEAD, CELL_DEAD,  CELL_DEAD),
+                                                     vec!(CELL_DEAD, CELL_ALIVE, CELL_DEAD),
+                                                     vec!(CELL_DEAD, CELL_DEAD,  CELL_DEAD)));
+    }
+
+    #[test]
+    fn fmt_should_format_grid_as_string() {
+        // given
+        let grid = Grid { 
+            cells:  vec!(vec!(CELL_ALIVE, CELL_ALIVE,  CELL_ALIVE),
+                         vec!(CELL_ALIVE, CELL_DEAD,   CELL_ALIVE),
+                         vec!(CELL_ALIVE, CELL_ALIVE,  CELL_ALIVE))
+        };
+
+        // when
+        let formatted = format!("{}", grid); 
+
+        // then
+        assert_eq!(formatted, "OOO\r\nO O\r\nOOO");
+    }
+
+    #[test]
+    fn index_wrap_should_return_correct_indices() {
+        assert_eq!(Grid::index_wrap(-3, 3), 0);
+        assert_eq!(Grid::index_wrap(-2, 3), 1);
+        assert_eq!(Grid::index_wrap(-1, 3), 2);
+        assert_eq!(Grid::index_wrap( 0, 3), 0);
+        assert_eq!(Grid::index_wrap( 1, 3), 1);
+        assert_eq!(Grid::index_wrap( 2, 3), 2);
+        assert_eq!(Grid::index_wrap( 3, 3), 0);
+    }
+
+/*****************************************************************************/
+
+    #[test]
+    fn to_char_should_return_correct_chars() {
+        assert_eq!(Cell { alive: false }.to_char(), ' ');
+        assert_eq!(Cell { alive: true }.to_char(),  'O');
+    }
+
+    #[test]
+    fn next_should_return_correct_next_state() {
+        // cell stays alive if 2 or 3 neighbours
+        assert_eq!(Cell::next(&CELL_ALIVE, 0).alive, false);
+        assert_eq!(Cell::next(&CELL_ALIVE, 1).alive, false);
+        assert_eq!(Cell::next(&CELL_ALIVE, 2).alive, true);
+        assert_eq!(Cell::next(&CELL_ALIVE, 3).alive, true);
+        assert_eq!(Cell::next(&CELL_ALIVE, 4).alive, false);
+        assert_eq!(Cell::next(&CELL_ALIVE, 5).alive, false);
+        assert_eq!(Cell::next(&CELL_ALIVE, 6).alive, false);
+        assert_eq!(Cell::next(&CELL_ALIVE, 7).alive, false);
+        assert_eq!(Cell::next(&CELL_ALIVE, 8).alive, false);
+
+        // cell stays dead unless 3 neighbours
+        assert_eq!(Cell::next(&CELL_DEAD, 0).alive, false);
+        assert_eq!(Cell::next(&CELL_DEAD, 1).alive, false);
+        assert_eq!(Cell::next(&CELL_DEAD, 2).alive, false);
+        assert_eq!(Cell::next(&CELL_DEAD, 3).alive, true);
+        assert_eq!(Cell::next(&CELL_DEAD, 4).alive, false);
+        assert_eq!(Cell::next(&CELL_DEAD, 5).alive, false);
+        assert_eq!(Cell::next(&CELL_DEAD, 6).alive, false);
+        assert_eq!(Cell::next(&CELL_DEAD, 7).alive, false);
+        assert_eq!(Cell::next(&CELL_DEAD, 8).alive, false);
+    }
+}
+
+/*****************************************************************************/
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,24 @@
-/* Terminal based implementation of John Conway's Game of Life 
-*  (c)2020 James A Wright, see LICENSE file.
+/* main.rs - Terminal based implementation of John Conway's Game of Life
+*  (c)2020 James Wright, see LICENSE file.
 */
 
-extern crate rand;
 extern crate termion;
+extern crate rand;
 
+use life::{Cell, Grid};
+use rand::{thread_rng};
 use std::{thread, time};
 use std::io::{Write, stdout};
-use rand::{thread_rng, Rng};
 use termion::{clear,color,cursor,style};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-/*****************************************************************************/
-
-const SLEEP_MILLIS: u64     = 100;
-const EMPTY_CELL: 	char 	= ' ';
-const LIVE_CELL: 	char 	= 'O';
+mod life;
 
 /*****************************************************************************/
 
-// Return next cell state
-fn cell_update(cell: bool, neighbours: u8) -> bool {
-    (neighbours == 3) || ((neighbours == 2) && cell)
-}
-
-// Return wrapped index making sure it's within range [0..len)
-fn grid_index_wrap(i: isize, len: usize) -> usize {
-    (i + len as isize) as usize % len
-}
-
-// Return number of neighbours for given cell position
-fn grid_neighbours(grid: &[Vec<bool>], x: usize, y: usize) -> u8 {
-    let left   = grid_index_wrap(x as isize - 1, grid[0].len());  // wrap to row width
-    let right  = grid_index_wrap(x as isize + 1, grid[0].len());  // wrap to row width
-    let top    = grid_index_wrap(y as isize - 1, grid.len());     // wrap to grid height
-    let bottom = grid_index_wrap(y as isize + 1, grid.len());     // wrap to grid height
-
-    (grid[top][left]     as u8) + 
-    (grid[top][x]        as u8) + 
-    (grid[top][right]    as u8) +
-    (grid[y][left]       as u8) + 
-    (grid[y][right]      as u8) +
-    (grid[bottom][left]  as u8) + 
-    (grid[bottom][x]     as u8) + 
-    (grid[bottom][right] as u8)
-}
-
-// Return next row of state states
-fn grid_update_row(grid: &[Vec<bool>], y: usize) -> Vec<bool> {
-    grid[y].iter().enumerate()
-        .map(|(x, cell)| cell_update(*cell, grid_neighbours(grid, x, y)))
-        .collect()
-}
-
-// Return next grid state
-fn grid_update(grid: &[Vec<bool>]) -> Vec<Vec<bool>> {
-    (0..grid.len())
-        .map(|y| grid_update_row(grid, y))
-        .collect()
-}
-
-// Render grid state to a String
-fn grid_to_string(grid: &[Vec<bool>]) -> String {
-    grid.iter()
-        .map(|row| row.iter().map(|x| if *x { LIVE_CELL } else { EMPTY_CELL } ).collect::<String>() )
-        .collect::<Vec<String>>()
-        .join("\r\n")
-}
-
-// Generate a grid of randomsied cell states (bools)
-fn grid_randomise(width: u16, height: u16) -> Vec<Vec<bool>> {
-    let mut rng = thread_rng();			// store rng locally for performance
-    (0..height)
-        .map(|_| (0..width).map(|_| rng.gen::<bool>()).collect())
-        .collect()
-}
+const SLEEP_MILLIS: u64 = 100;
 
 /*****************************************************************************/
 
@@ -119,8 +61,11 @@ fn main() {
     terminal.init();
     terminal.flush();
 
+    // Create a PRNG
+    let rng = thread_rng();
+
     // Initialise grid with randomised cell states
-    let mut grid: Vec<Vec<bool>> = grid_randomise(terminal.size.0, terminal.size.1);
+    let mut grid = Grid::random(rng, terminal.size.0 as usize, terminal.size.1 as usize);
 
     // Main loop
     loop {
@@ -131,7 +76,6 @@ fn main() {
             match key {
                 Key::Esc => break,
                 Key::Char('q') => break,
-                Key::Char('r') => { grid = grid_randomise(terminal.size.0, terminal.size.1) },
                 _ => {}
             }
         }
@@ -140,17 +84,15 @@ fn main() {
         let term_size = termion::terminal_size().unwrap();
         if term_size != terminal.size {
             terminal.size = term_size;
-            grid = grid_randomise(terminal.size.0, terminal.size.1);
+            grid = Grid::random(rng, terminal.size.0 as usize, terminal.size.1 as usize);
         }
 
-        // Update next grid state
-        grid = grid_update(&grid);            
+        // Get the next Grid state using the Cell::next function
+        grid = Grid::next(&grid, Cell::next);
 
         // Render to terminal
         terminal.write(&format!("{}{}{}{}{}", 
-            cursor::Goto(1, 1), style::Bold, color::Fg(color::Green), 
-            grid_to_string(&grid), 
-            style::Reset));
+            cursor::Goto(1, 1), style::Bold, color::Fg(color::Green), grid, style::Reset));
 
         // Flush terminal output
         terminal.flush();
@@ -162,63 +104,6 @@ fn main() {
     // Restore terminal and flush output
     terminal.restore();
     terminal.flush();
-}
-
-/*****************************************************************************/
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn cell_update_should_return_correct_next_state() {
-        // cell stays alive if 2 or 3 neighbours
-        assert_eq!(cell_update(true, 0), false);
-        assert_eq!(cell_update(true, 1), false);
-        assert_eq!(cell_update(true, 2), true);
-        assert_eq!(cell_update(true, 3), true);
-        assert_eq!(cell_update(true, 4), false);
-        assert_eq!(cell_update(true, 5), false);
-        assert_eq!(cell_update(true, 6), false);
-        assert_eq!(cell_update(true, 7), false);
-        assert_eq!(cell_update(true, 8), false);
-
-        // cell stays dead unless 3 neighbours
-        assert_eq!(cell_update(false, 0), false);       
-        assert_eq!(cell_update(false, 1), false);
-        assert_eq!(cell_update(false, 2), false);
-        assert_eq!(cell_update(false, 3), true);
-        assert_eq!(cell_update(false, 4), false);
-        assert_eq!(cell_update(false, 5), false);
-        assert_eq!(cell_update(false, 6), false);
-        assert_eq!(cell_update(false, 7), false);
-        assert_eq!(cell_update(false, 8), false);
-    }
-
-    #[test]
-    fn grid_index_wrap_should_return_correct_indices() {
-        assert_eq!(grid_index_wrap(-3, 3), 0);
-        assert_eq!(grid_index_wrap(-2, 3), 1);
-        assert_eq!(grid_index_wrap(-1, 3), 2);
-        assert_eq!(grid_index_wrap( 0, 3), 0);
-        assert_eq!(grid_index_wrap( 1, 3), 1);
-        assert_eq!(grid_index_wrap( 2, 3), 2);
-        assert_eq!(grid_index_wrap( 3, 3), 0);
-    }
-
-    #[test]
-    fn grid_randomise_should_generate_grid_with_correct_dimensions() {
-        // given
-        let width:  u16 = 7;
-        let height: u16 = 5;
-
-        // when
-        let grid: Vec<Vec<bool>> = grid_randomise(width, height);
-        
-        // then
-        assert_eq!(grid.len(), height as usize);
-        assert_eq!(grid.iter().any(|row| row.len() != width as usize), false);
-    }
 }
 
 /*****************************************************************************/
