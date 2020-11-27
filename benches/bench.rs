8141be28662b03bedfb9082f3f8d0b2ad82fe8e8
James Wright <james.wright@digital-chaos.com>
2020-11-27 19:11:22 +0000

    Add benchmarks for different size Grids

--- benches/bench.rs
+++ benches/bench.rs
@@ -11,22 +11,36 @@ use rs_life::{Cell,Grid};
 
 /*****************************************************************************/
 
-fn grid_random(bench: &mut Bencher) {
+fn grid_next_160_x_120(bench: &mut Bencher) {
     let rng = thread_rng();
-    bench.iter(|| { Grid::random(rng, 320, 240) })
+    let grid = Grid::random(rng, 160, 120);
+    bench.iter(|| { grid.next(Cell::next) })
 }
 
-fn grid_next(bench: &mut Bencher) {
+fn grid_next_320_x_240(bench: &mut Bencher) {
     let rng = thread_rng();
     let grid = Grid::random(rng, 320, 240);
     bench.iter(|| { grid.next(Cell::next) })
 }
 
+fn grid_next_640_x_480(bench: &mut Bencher) {
+    let rng = thread_rng();
+    let grid = Grid::random(rng, 640, 480);
+    bench.iter(|| { grid.next(Cell::next) })
+}
+
+fn grid_random(bench: &mut Bencher) {
+    let rng = thread_rng();
+    bench.iter(|| { Grid::random(rng, 320, 240) })
+}
+
 /*****************************************************************************/
 
 benchmark_group!(benches, 
-    grid_random, 
-    grid_next, 
+    grid_next_160_x_120,
+    grid_next_320_x_240,
+    grid_next_640_x_480,
+    grid_random,
 );
 benchmark_main!(benches);
 
